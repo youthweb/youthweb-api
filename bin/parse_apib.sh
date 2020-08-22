3e02ca7e5c8e6156fd54a5dddcd767ec377d5419
Art4 <art4@wlabs.de>
2020-08-22 22:19:12 +0200

    Debug + Nach Generierung von 0.17 und 0.16 aufräumen

--- bin/parse_apib.sh
+++ bin/parse_apib.sh
@@ -5,9 +5,11 @@ mkdir -p ./public/spec/core || { exit 1; }
 echo "Render HTML for next version"
 git fetch origin develop:develop || { exit 1; }
 git checkout develop -- ./spec || { exit 1; }
+git status
 cp -r ./spec ./public/spec/core/next || { exit 1; }
 git reset HEAD ./spec || { exit 1; }
 git checkout -- ./spec || { exit 1; }
+git status
 node_modules/.bin/aglio -i ./public/spec/core/next/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/next/index.html || { exit 1; }
 
 echo "Render HTML for 0.17"
@@ -15,6 +17,8 @@ git fetch origin v0.17:v0.17 || { exit 1; }
 git checkout v0.17 -- ./spec || { exit 1; }
 mkdir -p ./public/spec/core/0.17 || { exit 1; }
 cp -r ./spec ./public/spec/core/0.17 || { exit 1; }
+git reset HEAD ./spec || { exit 1; }
+git checkout -- ./spec || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/0.17/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.17/index.html || { exit 1; }
 
 echo "Render HTML for 0.16"
@@ -22,6 +26,8 @@ git fetch origin v0.16:v0.16 || { exit 1; }
 git checkout v0.16 -- ./spec || { exit 1; }
 mkdir -p ./public/spec/core/0.16 || { exit 1; }
 cp -r ./spec ./public/spec/core/0.16 || { exit 1; }
+git reset HEAD ./spec || { exit 1; }
+git checkout -- ./spec || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/0.16/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.16/index.html || { exit 1; }
 
 echo "Render HTML for 0.15"
