3dd6dd8eb50ecaad72408f4f1b9f4bf2dacea06f
Art4 <art4@wlabs.de>
2021-02-08 22:30:20 +0100

    branch 0.18 in parse_apib.sh aufgenommen
    refs #2504

--- bin/parse_apib.sh
+++ bin/parse_apib.sh
@@ -10,6 +10,14 @@ git reset HEAD ./spec || { exit 1; }
 git checkout -- ./spec || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/next/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/next/index.html || { exit 1; }
 
+echo "Render HTML for 0.18"
+git fetch origin v0.18:v0.18 || { exit 1; }
+git checkout v0.18 -- ./spec || { exit 1; }
+cp -r ./spec ./public/spec/core/0.18 || { exit 1; }
+git reset HEAD ./spec || { exit 1; }
+git checkout -- ./spec || { exit 1; }
+node_modules/.bin/aglio -i ./public/spec/core/0.18/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.18/index.html || { exit 1; }
+
 echo "Render HTML for 0.17"
 git fetch origin v0.17:v0.17 || { exit 1; }
 git checkout v0.17 -- ./spec || { exit 1; }
