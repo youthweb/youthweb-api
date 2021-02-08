781b14ffdd6607d8d7e1aec982182f67cfe4bb1c
Art4 <art4@wlabs.de>
2021-02-08 23:25:53 +0100

    aglio parsing für 0.15 und 0.16 auf tags geändert
    refs #2504

--- bin/parse_apib.sh
+++ bin/parse_apib.sh
@@ -27,16 +27,14 @@ git checkout -- ./spec || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/0.17/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.17/index.html || { exit 1; }
 
 echo "Render HTML for 0.16"
-git fetch origin v0.16:v0.16 || { exit 1; }
-git checkout v0.16 -- ./spec || { exit 1; }
+git checkout 0.16.0 -- apiary.apib || { exit 1; }
 cp -r ./spec ./public/spec/core/0.16 || { exit 1; }
 git reset HEAD ./spec || { exit 1; }
 git checkout -- ./spec || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/0.16/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.16/index.html || { exit 1; }
 
 echo "Render HTML for 0.15"
-git fetch origin v0.15:v0.15 || { exit 1; }
-git checkout v0.15 -- apiary.apib || { exit 1; }
+git checkout 0.15.2 -- apiary.apib || { exit 1; }
 mkdir -p ./public/spec/core/0.15 || { exit 1; }
 mv ./apiary.apib ./public/spec/core/0.15/apiary.apib || { exit 1; }
 node_modules/.bin/aglio -i ./public/spec/core/0.15/apiary.apib --no-theme-condense --theme-full-width -o ./public/spec/core/0.15/index.html || { exit 1; }
